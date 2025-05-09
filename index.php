ae30a6b79c24fd68c7267f9098f69571f5024595
iam5hakeel <shykiahmad@gmail.com>
2025-05-09 03:36:29 +0100

    test

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Skill for All LMS | Learning Management System</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" type="image/x-icon" href="logo.png">
+    
+    <style>
+        body, html {
+            height: 100%;
+            margin: 0;
+        }
+        iframe {
+            width: 100%;
+            height: 100%;
+            border: none;
+        }
+    </style>
+</head>
+<body>
+    <iframe src="https://lms.facilesol.co.uk/" title="Skill for All Program"></iframe>
+</body>
+</html>
